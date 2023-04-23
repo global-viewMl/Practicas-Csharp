0da3a9207d52318adfcc9f02ae3934da7f222485
global-viewMl <67601092+global-viewMl@users.noreply.github.com>
2023-04-22 20:56:34 -0400

    Comienzo de las practicas y prueba de git

---
+++ Practicas Csharp/Practicas Csharp/Program.cs
@@ -0,0 +1,14 @@
+namespace Practicas_Csharp
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            
+        }
+    }
+}
